91c2e598cd4dc5dc47e38d15febba85afb7e6017
hello-world-ttj <tijotjoseph003@gmail.com>
2024-05-17 19:33:38 +0530

    #clean

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,24 +1,9 @@
-import Navbar from "@/components/Navbar";
-import ServiceCard from "@/components/ServiceCard";
-import StyledAccordion from "@/components/StyledAccordion";
-import ViewMoreCard from "@/components/ViewMoreCard";
-import StyledInput from "@/ui/StyledInput";
-import StyledPhoneInput from "@/ui/StyledPhoneInput";
-import StyledRadioButton from "@/ui/StyledRadioButton";
-import StyledSelect from "@/ui/StyledSelect";
-import StyledTabs from "@/ui/StyledTabs";
-import StyledTextArea from "@/ui/StyledTextArea";
-import { Button, Typography, Box } from "@mui/material";
+import React from 'react'
 
-export default function Page() {
-  const options = [
-    { label: "option1 ", value: 1 },
-    { label: "option2", value: 2 },
-    { label: "option3", value: 3 },
-    { label: "option", value: 4 },
-  ];
-
+function page() {
   return (
-  <div>Hello</div>
-  );
+    <div>page</div>
+  )
 }
+
+export default page
