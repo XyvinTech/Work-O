136a6dbc507e228e913cb2265ff39bc3615ac60e
hello-world-ttj <tijotjoseph003@gmail.com>
2024-05-20 19:05:55 +0530

    #WO-23

--- src/app/get-the-app/page.jsx
+++ src/app/get-the-app/page.jsx
@@ -2,12 +2,14 @@ import Header from "@/components/Header";
 import React from "react";
 import bgImage from "/public/assets/get-the-app/img/header.png";
 import GetApp from "@/components/GetApp";
+import Achievements from "@/components/Achievements";
 
 function page() {
   return (
     <>
       <Header title={"Download our Worko App"} bgImg={bgImage.src} />
       <GetApp />
+      <Achievements />
     </>
   );
 }
---
+++ src/components/Achievements.jsx
@@ -0,0 +1,34 @@
+"use client"
+import { Grid, Stack, Typography, useMediaQuery, useTheme } from "@mui/material";
+
+const awards = [
+  { src: "/assets/award/award1.png", alt: "award1" },
+  { src: "/assets/award/award2.png", alt: "award2" },
+  { src: "/assets/award/award3.png", alt: "award3" },
+  { src: "/assets/award/award4.png", alt: "award4" },
+  { src: "/assets/award/award5.png", alt: "award5" },
+  { src: "/assets/award/award2.png", alt: "award2" },
+  { src: "/assets/award/award1.png", alt: "award1" },
+  { src: "/assets/award/award2.png", alt: "award2" },
+];
+
+function Achievements() {
+    const theme = useTheme();
+    const isMobile = useMediaQuery(theme.breakpoints.down("sm"));
+  return (
+    <Stack bgcolor={"#FBF8F2"} p={isMobile ? 4 : 8} spacing={10}>
+      <Typography variant="h2" textAlign={"center"}>
+        Our Achievements & Awards
+      </Typography>
+      <Grid container  justifyContent="space-between" spacing={isMobile && 4}>
+        {awards.map((award, index) => (
+          <Grid key={index} item xs={3} sm={1} sx={{ height: { xs: 60, sm: 120 } }}>
+            <img src={award.src} alt="award" height="100%" />
+          </Grid>
+        ))}
+      </Grid>
+    </Stack>
+  );
+}
+
+export default Achievements;
