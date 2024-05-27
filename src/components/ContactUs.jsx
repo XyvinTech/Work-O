2d1cda7b6fc8862a8833dba01419a898fc000360
anjanavu <anjanavu2000@gmail.com>
2024-05-27 20:57:19 +0530

    updation

--- src/components/ContactUs.jsx
+++ src/components/ContactUs.jsx
@@ -68,7 +68,7 @@ const Container = styled(Box)(() => ({
     left: 0,
     right: 0,
     bottom: 0,
-    backgroundColor: "rgba(0, 0, 0, 0.9)",
+    backgroundColor: "rgba(0, 0, 0, 0.95)",
     zIndex: 1,
   },
   "& > *": {
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -31,7 +31,7 @@ const FooterContainer = styled(Box)(() => ({
     left: 0,
     right: 0,
     bottom: 0,
-    backgroundColor: "rgba(0, 0, 0, 0.9)",
+    backgroundColor: "rgba(0, 0, 0, 0.95)",
     zIndex: 1,
   },
   "& > *": {
