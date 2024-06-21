58286d8a58d94d6b54a8922282b4e5e5b6cfa765
anjanavu <anjanavu2000@gmail.com>
2024-06-21 21:45:07 +0530

    contact page

--- src/components/TrainingProgram.jsx
+++ src/components/TrainingProgram.jsx
@@ -1,37 +1,23 @@
 "use client";
 import viewMoreData from "../assets/json/TrainingProgramData";
-import { Box } from "@mui/material";
+import { Box, Grid } from "@mui/material";
 import styled from "styled-components";
 import TrainCard from "./TrainCard";
 
-
-const ViewMoreContainer = styled(Box)`
-  display: flex;
-  flex-wrap: nowrap;
-  justify-content: space-between;
-  gap: 15px;
-  overflow-x: hidden;
-
-  @media (max-width: 600px) {
-    flex-wrap: wrap;
-    flex-direction: row;
-    overflow-x: hidden;
-  }
-`;
-
 const TrainingProgram = () => {
   return (
-    <ViewMoreContainer>
+    <Grid container spacing={2}>
       {viewMoreData.map((item, index) => (
+        <Grid key={index} item md={4} xs={12}>
         <TrainCard
           key={index}
           image={item.image}
           title={item.title}
           description={item.description}
           
-        />
+        /></Grid>
       ))}
-    </ViewMoreContainer>
+  </Grid>
   );
 };
 
