52b6347693c776d90c60dc8952db6e20b50e09a4
anjanavu <anjanavu2000@gmail.com>
2024-08-19 18:03:48 +0530

    loader

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -6,23 +6,16 @@ import ContactUs from "@/components/ContactUs";
 import StyledLoader from "@/components/StyledLoader";
 
 function page() {
-  const [isLoading, setIsLoading] = useState(true);
-  useEffect(() => {   
-    setTimeout(() => {
-      setIsLoading(false);
-    }, 2000);
-  }, []);
+
   return (
     <>
      
       <Header title={"Contact Us"}top={15} bgImg={bgImage.src}  mobile={"280px"}height={"400px"}/>
-      {isLoading ? (
-        <StyledLoader />
-      ) : (
+      
         <>
       <ContactUs />
     </>
-     )}
+    
     </>
   );
 }
