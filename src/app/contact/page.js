1151fb9eeeea37ad21244d353bf3f16255f58bde
anjanavu <anjanavu2000@gmail.com>
2024-11-07 14:40:40 +0530

    #scroll

--- src/app/contact/page.js
+++ src/app/contact/page.js
@@ -1,7 +1,12 @@
+"use client";
 import Header from "@/components/Header";
 import bgImage from "/public/assets/contact/img/2.png";
 import ContactUs from "@/components/ContactUs";
+import { useEffect } from "react";
 export default function page() {
+  useEffect(() => {
+    window.scrollTo(0, 0); 
+  }, []); 
   return (
     <>
       {" "}
